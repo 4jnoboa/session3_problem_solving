99d2b0d992bcc1f09317d71ad8dbb6d16f964f68
jose noboa <j.noboa@outlook.com>
2024-01-22 14:05:26 -0500

    initial

---
+++ problem_solving.rb
@@ -0,0 +1,58 @@
+# challege 1
+#name = "jose"
+#puts name.count.even? "jose"
+
+# challenge 2
+#Update your previous method or function so it accepts an array. The method or function should return a 
+#boolean that describes whether or not the array's length is an even number.
+
+#name = "jose"
+
+#puts name.length
+
+
+
+
+#challenge 3
+
+
+time = 1600
+if time >= 400 && time <= 1100
+    puts "good morning"
+
+elsif time >=1200 && time <= 1600
+    puts "good afternoon"
+
+elsif time >= 1700 && time <= 2000
+    puts "good evening"
+
+else time >=2100 && time <= 300
+    puts "good night"
+
+end
+
+
+#challenge 4
+
+#numbers = [1,2,3\-,4,5,6,7,8,9,4]
+#numbers = [1,2,3,4,5,6,7,8,9,4]
+#phone_number = numbers.join
+#puts phone_number.insert(3, "-",) + puts phone_number.insert(6,"-")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
