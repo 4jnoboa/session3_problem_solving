7c61920af85c4bac151db4eb051ffde9b1db4835
jose noboa <j.noboa@outlook.com>
2024-01-22 14:07:25 -0500

    hw revision

--- problem_solving.rb
+++ problem_solving.rb
@@ -1,9 +1,12 @@
+#https://mod0.turing.edu/shared/problem-solving
+
 # challege 1
 #name = "jose"
 #puts name.count.even? "jose"
 
 # challenge 2
-#Update your previous method or function so it accepts an array. The method or function should return a 
+#Update your previous method or function so it accepts an array. The method or function should 
+#return a 
 #boolean that describes whether or not the array's length is an even number.
 
 #name = "jose"
@@ -33,11 +36,21 @@ end
 
 
 #challenge 4
+#Challenge 4 - OPTIONAL! Spicy!
+#Write a method or function that accepts an array of 10 integers (between 0 and 9) and 
+#returns a string of those numbers in the form of a phone number. For example, 
+#if you were given [5, 5, 5, 1, 2, 3, 4, 5, 6, 7], the method or function will return 555-123-4567.
 
 #numbers = [1,2,3\-,4,5,6,7,8,9,4]
 #numbers = [1,2,3,4,5,6,7,8,9,4]
 #phone_number = numbers.join
 #puts phone_number.insert(3, "-",) + puts phone_number.insert(6,"-")
+
+# Challenge 5 - OPTIONAL! Super Spicy!
+#Start with the array of strings below. Print all of the words in the array, 
+#but change every t to an uppercase T.
+
+#Starter array: ['cat', 'dog', 'parrot', 'turtle', 'bird']
 
 
 
