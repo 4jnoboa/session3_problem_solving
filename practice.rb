22781a0396a8b01cabc8cd1c9f144d4e8980da56
jose noboa <j.noboa@outlook.com>
2024-01-22 19:50:43 -0500

    refractor challenge 2 and 3

--- practice.rb
+++ practice.rb
@@ -3,15 +3,33 @@
 #answer = name.length 
 #puts answer.even?
 
-def is_answer_even(word)      # im defining my method  ( word is my parameter)
-     word.length.even?        # i am writing my code block 
+#def is_answer_even(word)      # im defining my method  ( word is my parameter)
+     #word.length.even?        # i am writing my code block 
+#end
+#puts is_answer_even("tom")    # i am invoking my method / i am calling my method # "tom is my argument"
+
+# challenge 2
+#Update your previous method or function so it accepts an array.
+ #The method or function should return a boolean that describes 
+ #whether or not the array's length is an even number.
+
+
+#puts is_answer_even(["tom","jose",]) 
+
+def greet_person(time) 
+    if time >= 400 && time <= 1100
+        "good morning"
+    elsif time >=1200 && time <= 1600
+        "good afternoon"
+    elsif time >= 1700 && time <= 2000
+        "good evening"
+    elsif time >=2100 && time <= 300
+        "good night"
+    else 
+        "that is not a valid time"
+    end
 end
-puts is_answer_even("tom")    # i am invoking my method / i am calling my method # "tom is my argument"
-
-
-
-
-
+puts greet_person(1800)
 
 
 
--- problem_solving.rb
+++ problem_solving.rb
@@ -22,27 +22,34 @@
 #puts answer.even?
 
 #puts name.length
-
+def is_answer_even(word)      # im defining my method  ( word is my parameter)
+    word.length.even?         # i am writing my code block 
+end
+puts is_answer_even(["tom","jose",]) 
+# laura helped me a lot here. 
+# the lentgh method after a parameter can be used on strings or arrays,
+# and even will give us a boolean based on the lentgh . the lentgh coverts
+# the answer to an integer
 
 
 
 #challenge 3
 
 
-time = 1600
-if time >= 400 && time <= 1100
-    puts "good morning"
-
-elsif time >=1200 && time <= 1600
-    puts "good afternoon"
-
-elsif time >= 1700 && time <= 2000
-    puts "good evening"
-
-else time >=2100 && time <= 300
-    puts "good night"
-
+def greet_person(time) 
+    if time >= 400 && time <= 1100
+        "good morning"
+    elsif time >=1200 && time <= 1600
+        "good afternoon"
+    elsif time >= 1700 && time <= 2000
+        "good evening"
+    elsif time >=2100 && time <= 300
+        "good night"
+    else 
+        "that is not a valid time"
+    end
 end
+puts greet_person(1800)
 
 
 #challenge 4
